3dc44eb901b9619ade4c33063605b5180c143477
celestebickham <90701404+celestebickham@users.noreply.github.com>
2023-04-21 08:45:18 -0500

    Prep 2 first step

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+
+        float x = float.Parse(percentage);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratualtions! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class.");
+        }
     }
 }
