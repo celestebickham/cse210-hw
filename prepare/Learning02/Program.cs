5cc6ddd4bc4a9819a2c54f6197c8f08a71261647
celestebickham <90701404+celestebickham@users.noreply.github.com>
2023-04-26 12:56:29 -0500

    Program2 Resume and Job

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Simplot";
+        job1._startYear = 2015;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Software Engineer";
+        job2._company = "Walmart";
+        job2._startYear = 2019;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Celeste Bickham";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
